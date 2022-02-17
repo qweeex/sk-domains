49f4901223c7aa94603b7a69c23c8b69b8ee5c5a
Arkady Pugachev <qweeex@yandex.com>
2022-02-17 18:47:07 +0400

    release v1

---
+++ dist/Manager/WebManager.js
@@ -0,0 +1,25 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+exports.__esModule = true;
+var express_1 = __importDefault(require("express"));
+var WebManager = /** @class */ (function () {
+    function WebManager() {
+        this.ExpressCore = express_1["default"]();
+        // @ts-ignore
+        this.ExpressCore.use(express_1["default"].json());
+        this.InitRouter();
+    }
+    WebManager.prototype.InitRouter = function () {
+        this.ExpressCore.get('/', function (req, res) {
+            res.send('asd');
+        });
+    };
+    WebManager.prototype.Start = function () {
+        this.ExpressCore.listen(8080);
+    };
+    WebManager.Instance = new WebManager();
+    return WebManager;
+}());
+exports["default"] = WebManager;
---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+exports.__esModule = true;
+var WebManager_1 = __importDefault(require("./Manager/WebManager"));
+WebManager_1["default"].Instance.Start();
